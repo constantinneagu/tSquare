826aebfcc55567d49cd1e4eb0a8cef63a775be61
Constantin Neagu <constantin.neagu.office@gmail.com>
2016-03-14 16:38:10 +0200

    NP 0.12

--- tSquareAdmin/routes/gallery.js
+++ tSquareAdmin/routes/gallery.js
@@ -61,7 +61,7 @@ router.get('/pictures', function (req, res, next) {
 		'metadata.aspectRatio' : true
 	}).toArray(function (err, thumbnails) {
 		assert.equal(null, err);
-		console.log(thumbnails);
+		// // console.log(thumbnails);
 		res.json(thumbnails);
 	});
 });
@@ -93,7 +93,7 @@ function checkEtag(req, res, next) {
 };
 /* File retrieve and send method */
 function getFile(req, res) {
-	console.log(req.fileName + ' bla');
+	// console.log(req.fileName + ' bla');
 	assert.notEqual(null, req.fileName);
 	assert.notEqual(null, req.collection);
 
@@ -108,10 +108,10 @@ function getFile(req, res) {
 			});
 		file.open(function (err, file) {
 			assert.equal(null, err);
-			console.log('file.open');
+			// console.log('file.open');
 			var fileStream = file.stream();
 			fileStream.on('end', function () {
-				console.log('fileStream.on');
+				// console.log('fileStream.on');
 				res.end();
 			});
 			res.set({
@@ -125,7 +125,7 @@ function getFile(req, res) {
 
 /* Thumbnail retrieve method  */
 router.get('/pictures/thumbnail/:thumbnail', function (req, res, next) {
-	console.log(req.params.thumbnail + ' bla');
+	// console.log(req.params.thumbnail + ' bla');
 	assert.notEqual(null, req.params.thumbnail);
 	req.fileName = req.params.thumbnail;
 	req.collection = resolutions[0].name;
@@ -135,7 +135,7 @@ router.get('/pictures/thumbnail/:thumbnail', function (req, res, next) {
 
 /* Original retrieve method  */
 router.get('/pictures/original/:original', function (req, res, next) {
-	console.log(req.params.original + ' bla');
+	// console.log(req.params.original + ' bla');
 	assert.notEqual(null, req.params.original);
 	req.fileName = req.params.original;
 	req.collection = resolutions[5].name;
@@ -145,7 +145,7 @@ router.get('/pictures/original/:original', function (req, res, next) {
 
 /* Xga retrieve method  */
 router.get('/pictures/xga/:xga', function (req, res, next) {
-	console.log(req.params.xga + ' bla');
+	// console.log(req.params.xga + ' bla');
 	assert.notEqual(null, req.params.xga);
 	req.fileName = req.params.xga;
 	req.collection = resolutions[1].name;
@@ -155,7 +155,7 @@ router.get('/pictures/xga/:xga', function (req, res, next) {
 
 /* Wxga retrieve method  */
 router.get('/pictures/wxga/:wxga', function (req, res, next) {
-	console.log(req.params.wxga + ' bla');
+	// console.log(req.params.wxga + ' bla');
 	assert.notEqual(null, req.params.wxga);
 	req.fileName = req.params.wxga;
 	req.collection = resolutions[2].name;
@@ -165,7 +165,7 @@ router.get('/pictures/wxga/:wxga', function (req, res, next) {
 
 /* Hd retrieve method  */
 router.get('/pictures/hd/:hd', function (req, res, next) {
-	console.log(req.params.hd + ' bla');
+	// console.log(req.params.hd + ' bla');
 	assert.notEqual(null, req.params.hd);
 	req.fileName = req.params.hd;
 	req.collection = resolutions[3].name;
@@ -175,7 +175,7 @@ router.get('/pictures/hd/:hd', function (req, res, next) {
 
 /* fhd retrieve method  */
 router.get('/pictures/fhd/:fhd', function (req, res, next) {
-	console.log(req.params.fhd + ' bla');
+	// console.log(req.params.fhd + ' bla');
 	assert.notEqual(null, req.params.fhd);
 	req.fileName = req.params.fhd;
 	req.collection = resolutions[4].name;
@@ -189,7 +189,7 @@ router.post('/pictures/insert', function (req, res, next) {
 	var collectionTarget,
 	aspectRatio = null,
 	pictureGalleryTags = [];
-	console.log(req.headers);
+	// console.log(req.headers);
 
 	var busboy = new Busboy({
 			headers : req.headers,
@@ -197,7 +197,7 @@ router.post('/pictures/insert', function (req, res, next) {
 			highWaterMark : 1024 * 1024
 		});
 	busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated) {
-		console.log('Fieldname [' + fieldname + ']: val: ' + val);
+		// console.log('Fieldname [' + fieldname + ']: val: ' + val);
 		if (fieldname === 'aspectRatio') {
 			aspectRatio = val;
 		} else {
@@ -207,7 +207,7 @@ router.post('/pictures/insert', function (req, res, next) {
 				if (fieldname === 'pictureGalleryTags') {
 					pictureGalleryTags = val.split(",");
 				} else {
-					console.log('Unknown parameter in request');
+					// console.log('Unknown parameter in request');
 					res.writeHead(404, {
 						Connection : 'close',
 						Location : '/gallery'
@@ -218,7 +218,7 @@ router.post('/pictures/insert', function (req, res, next) {
 		}
 	});
 	busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-		console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+		// console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
 		if (fieldname === 'blob') {
 			// Add the new metadata.
 			var pictures = new GridStore(db, filename, 'w', {
@@ -232,23 +232,23 @@ router.post('/pictures/insert', function (req, res, next) {
 				assert.equal(null, err);
 				file.on('data', function (data) {
 					file.pause();
-					//console.log('File [' + filename + '] got ' + data.length + ' bytes');
+					//// console.log('File [' + filename + '] got ' + data.length + ' bytes');
 					pictures.write(data, function (err, pictures) {
 						assert.equal(null, err);
-						//console.log('data written');
+						//// console.log('data written');
 						file.resume();
 					});
 				});
 				file.on('end', function () {
-					console.log('File [' + filename + '] Finished');
+					// console.log('File [' + filename + '] Finished');
 					pictures.close(function (err, result) {
 						assert.equal(null, err);
-						console.log('File written');
+						// console.log('File written');
 					});
 				});
 			});
 		} else {
-			console.log('Unknown parameter in request');
+			// console.log('Unknown parameter in request');
 			res.writeHead(404, {
 				Connection : 'close',
 				Location : '/gallery'
@@ -257,7 +257,7 @@ router.post('/pictures/insert', function (req, res, next) {
 		}
 	});
 	busboy.on('finish', function () {
-		console.log('Done parsing form!');
+		// console.log('Done parsing form!');
 		res.writeHead(200, {
 			Connection : 'close',
 			Location : '/gallery'
@@ -269,7 +269,7 @@ router.post('/pictures/insert', function (req, res, next) {
 
 router.post('/pictures/delete', function (req, res, next) {
 	var picture = req.body.picture;
-	console.log(picture);
+	// console.log(picture);
 	assert.notEqual(null, picture);
 	var db = require('../databases/tSquareMongoDB.js').db();
 	var GridStore = require('mongodb').GridStore;
@@ -279,7 +279,7 @@ router.post('/pictures/delete', function (req, res, next) {
 	function existsCallback(err, result) {
 		assert.equal(null, err);
 		assert.equal(true, result);
-		console.log('File ' + picture + ' exists in collection: ' + resolutions[resolutionIndex].name);
+		// console.log('File ' + picture + ' exists in collection: ' + resolutions[resolutionIndex].name);
 		if (resolutionIndex < resolutionsNo - 1) {
 			resolutionIndex++;
 			GridStore.exist(db, picture, resolutions[resolutionIndex].name, existsCallback);
@@ -288,11 +288,11 @@ router.post('/pictures/delete', function (req, res, next) {
 			resolutionIndex = 0;
 			function openAndDelete(err, pictures) {
 				assert.equal(null, err);
-				console.log(resolutionIndex);
-				console.log('Starting to delete : ' + resolutions[resolutionIndex].name);
+				// console.log(resolutionIndex);
+				// console.log('Starting to delete : ' + resolutions[resolutionIndex].name);
 				pictures.unlink(function (err, result) {
 					assert.equal(null, err);
-					console.log('Deleted file: ' + picture + ' from collection: ' + resolutions[resolutionIndex].name);
+					// console.log('Deleted file: ' + picture + ' from collection: ' + resolutions[resolutionIndex].name);
 					if (resolutionIndex < resolutionsNo - 1) {
 						resolutionIndex++;
 						pictures = new GridStore(db, picture, 'w', {
